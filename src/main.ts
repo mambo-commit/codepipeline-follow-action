b9e26726d37760ee9cb6174205be639acb03462a
leonardo rodriguez <Leonardo.Rodriguez@siriusxm.com>
2022-11-17 13:20:19 -0400

    remove async

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import * as core from '@actions/core'
 import {wait} from './wait'
 
-async function run(): Promise<void> {
+function run(): void {
   try {
     const ms: string = core.getInput('milliseconds')
     console.log(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
+    wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
