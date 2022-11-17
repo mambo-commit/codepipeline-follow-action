39f7b0446fd627ea1507e621b01cf302f0676466
leonardo rodriguez <Leonardo.Rodriguez@siriusxm.com>
2022-11-17 15:15:37 -0400

    add pipeline function

--- src/codepipeline.ts
+++ src/codepipeline.ts
@@ -1,10 +1,6 @@
 import * as aws_sdk from 'aws-sdk'
 
 export function pipelinestatus(pipelineName: string, accessKeyId: string, secretAccessKey: string): void {
-    const params = {
-        name: 'STRING_VALUE', 
-        version: 'NUMBER_VALUE'
-    };
 
     const credentials = new aws_sdk.Credentials({
         accessKeyId,
@@ -17,11 +13,18 @@ export function pipelinestatus(pipelineName: string, accessKeyId: string, secret
         credentials 
     })
     
-    const pipelineDetails = codePipeline.getPipeline({
+    const params = {
         name: pipelineName,
-    }, function(err, data){
+    }
+    console.log(`Pipeline name: ${pipelineName}`)
+    codePipeline.getPipeline(params, function(err, data){
         if (err) console.log(err, err.stack)
-        else console.log(data)
+        else console.log(`Pipeline ARN: ${data.metadata?.pipelineArn}`)
+    })
+
+    codePipeline.getPipelineState(params, function(err, data){
+        if (err) console.log(err, err.stack)
+        else console.log(`Pipeline state: ${data.stageStates?.at(0)?.inboundExecution?.status}`)
     })
 }
   
