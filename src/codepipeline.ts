a1290f6f6a84aff230f9d729c23faa2017741f57
leonardo rodriguez <Leonardo.Rodriguez@siriusxm.com>
2022-11-17 14:44:41 -0400

    add pipeline function

--- src/codepipeline.ts
+++ src/codepipeline.ts
@@ -1,6 +1,26 @@
-// import * as aws_sdk from 'aws-sdk'
+import * as aws_sdk from 'aws-sdk'
 
-// export async function pipelinestatus(pipelineName: string): Promise<void> {
-//     aws_sdk.CodePipeline
-//   }
+export function pipelinestatus(pipelineName: string, accessKeyId: string, secretAccessKey: string): object {
+    const params = {
+        name: 'STRING_VALUE', 
+        version: 'NUMBER_VALUE'
+    };
+
+    const credentials = new aws_sdk.Credentials({
+        accessKeyId,
+        secretAccessKey
+     })
+
+    const codePipeline = new aws_sdk.CodePipeline({
+        apiVersion: 'latest',
+        region: 'us-east-1',
+        credentials 
+    })
+    
+    const pipelineDetails = codePipeline.getPipeline({
+        name: pipelineName,
+    })
+
+    return pipelineDetails
+}
   
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import * as core from '@actions/core'
 import {wait} from './wait'
+import {pipelinestatus} from './codepipeline'
 
 function run(): void {
   try {
     const ms: string = core.getInput('milliseconds')
+    const pipelineName: string = core.getInput('pipelineName')
+    const accessKeyId: string = core.getInput('accessKeyId')
+    const secretAccessKey: string = core.getInput('secretAccessKey')
     console.log(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     core.debug(new Date().toTimeString())
     wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
+    console.log(pipelinestatus(pipelineName, accessKeyId, secretAccessKey))
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
