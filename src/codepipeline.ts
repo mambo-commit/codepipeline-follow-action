3e4e96bf021fcd0f26e322bc1a7bcffeecfde56e
leonardo rodriguez <Leonardo.Rodriguez@siriusxm.com>
2022-11-17 13:10:22 -0400

    test

--- src/codepipeline.ts
+++ src/codepipeline.ts
@@ -1,6 +1,6 @@
-import * as aws_sdk from 'aws-sdk'
+// import * as aws_sdk from 'aws-sdk'
 
-export async function pipelinestatus(pipelineName: string): Promise<void> {
-    aws_sdk.CodePipeline
-  }
+// export async function pipelinestatus(pipelineName: string): Promise<void> {
+//     aws_sdk.CodePipeline
+//   }
   
