a00ec4d22a5808e7175c395825f2ca398dd0bb51
leonardo rodriguez <Leonardo.Rodriguez@siriusxm.com>
2022-11-18 12:47:35 -0400

    disabling github actions

--- src/codepipeline.ts
+++ src/codepipeline.ts
@@ -1,51 +1,73 @@
+import {RepositoryNotificationEvents} from 'aws-cdk-lib/aws-codecommit'
 import * as aws_sdk from 'aws-sdk'
 
-export function pipelinestatus(pipelineName: string, accessKeyId: string, secretAccessKey: string): void {
+export function pipelinestatus(
+  pipelineName: string,
+  accessKeyId: string,
+  secretAccessKey: string,
+  bucketName: string,
+  artifactName: string
+): void {
+  const credentials = new aws_sdk.Credentials({
+    accessKeyId,
+    secretAccessKey
+  })
 
-    const credentials = new aws_sdk.Credentials({
-        accessKeyId,
-        secretAccessKey
-     })
+  let params
+  params = {
+    apiVersion: 'latest',
+    region: 'us-east-1',
+    credentials
+  }
 
-    const codePipeline = new aws_sdk.CodePipeline({
-        apiVersion: 'latest',
-        region: 'us-east-1',
-        credentials 
-    })
-    
-    let params = {
-        name: pipelineName,
-    }
+  const codePipeline = new aws_sdk.CodePipeline(params)
 
-    console.log(`Pipeline name: ${pipelineName}`)
-    codePipeline.getPipeline(params, function(err, data){
-        if (err) console.log(err, err.stack)
-        else console.log(`Pipeline ARN: ${data.metadata?.pipelineArn}`)
-    })
+  const artifactObject = new aws_sdk.S3(params)
 
-    // codePipeline.getPipelineState(params, function(err, data){
-    //     if (err) console.log(err, err.stack)
-    //     else console.log(`Pipeline state: ${data.stageStates?.at(0)?.stageName}`)
-    // })
-    
-    let pipelineExecutions
-    
-    codePipeline.listPipelineExecutions({
+  params =  {
+    name: pipelineName
+  }
+
+  console.log(`Pipeline name: ${pipelineName}`)
+  codePipeline.getPipeline(params, function (err, data) {
+    if (err) console.log(err, err.stack)
+    else console.log(`Pipeline ARN: ${data.metadata?.pipelineArn}`)
+  })
+
+  // codePipeline.getPipelineState(params, function(err, data){
+  //     if (err) console.log(err, err.stack)
+  //     else console.log(`Pipeline state: ${data.stageStates?.at(0)?.stageName}`)
+  // })
+
+  
+  artifactObject.getObjectAttributes({
+    Bucket: bucketName,
+    Key: artifactName,
+    ObjectAttributes: []
+  },
+  function(err) {
+    if (err) console.log(err, err.stack)
+  })
+  .on('success', function(response){
+    console.log(response.data)
+  })
+  
+  codePipeline
+    .listPipelineExecutions(
+      {
         pipelineName,
         maxResults: 1
-    }, function(err, data){
+      },
+      function (err) {
         if (err) console.log(err, err.stack)
-    }).on('success', function(response){
-        console.log(response.data)
-        pipelineExecutions = response.data
+      }
+    )
+    .on('success', function (response) {
+      console.log(response.data)
     })
 
-    console.log(pipelineExecutions)
-    
-    // console.log(`Pipeline Execution ID: ${data.pipelineExecutionSummaries?.at(0)?.pipelineExecutionId}`)
-    // console.log(`Pipeline SourceRevisions  ID: ${data.pipelineExecutionSummaries?.at(0)?.sourceRevisions?.at(0)?.revisionId}`)
-    // console.log(`Pipeline Execution StartTime: ${data.pipelineExecutionSummaries?.at(0)?.startTime}`)
-    // console.log(`Pipeline Execution Status: ${data.pipelineExecutionSummaries?.at(0)?.status}`)
-    
+  // console.log(`Pipeline Execution ID: ${data.pipelineExecutionSummaries?.at(0)?.pipelineExecutionId}`)
+  // console.log(`Pipeline SourceRevisions  ID: ${data.pipelineExecutionSummaries?.at(0)?.sourceRevisions?.at(0)?.revisionId}`)
+  // console.log(`Pipeline Execution StartTime: ${data.pipelineExecutionSummaries?.at(0)?.startTime}`)
+  // console.log(`Pipeline Execution Status: ${data.pipelineExecutionSummaries?.at(0)?.status}`)
 }
-  
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ function run(): void {
   try {
     const ms: string = core.getInput('milliseconds')
     const pipelineName: string = core.getInput('pipelineName')
+    const bucketName: string = core.getInput('bucketName')
+    const artifactName: string = core.getInput('artifactName')
     const accessKeyId: string = core.getInput('accessKeyId')
     const secretAccessKey: string = core.getInput('secretAccessKey')
     console.log(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
@@ -13,7 +15,7 @@ function run(): void {
     core.debug(new Date().toTimeString())
     wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
-    pipelinestatus(pipelineName, accessKeyId, secretAccessKey)
+    pipelinestatus(pipelineName, accessKeyId, secretAccessKey, bucketName, artifactName )
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
