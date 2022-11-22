f95754abb0712d829439c1c0120c039a2abeb502
leonardo rodriguez <Leonardo.Rodriguez@siriusxm.com>
2022-11-22 16:14:20 -0400

    Getting execution revision

--- src/codepipeline.ts
+++ src/codepipeline.ts
@@ -1,5 +1,5 @@
 import * as aws_sdk from 'aws-sdk'
-import { wait } from './wait'
+import {wait} from './wait'
 
 export function bucketVersion(
   accessKeyId: string,
@@ -28,93 +28,98 @@ export function bucketVersion(
     Prefix: artifactName,
     MaxKeys: 1
   }
+
+  const workFlow = async function () {
+    let timeOut = 3
+    let ms = 5000
+    do {
+      if (await excutionRevisionCheck(credentials, bucketName, artifactName)) {
+        console.log('Pipeline execution found!')
+        do {
+          //console.log(`Pipeline current status: ${await getStatus()}`)
+          await wait(2000)
+        } while (await isRunning(credentials))
+        console.log('Pipeline execution status check done')
+        break
+      }
+      await wait(ms)
+      timeOut -= 1
+    } while (timeOut > 0)
+  }
+
+  workFlow()
+}
+
+async function excutionRevisionCheck(
+  credentials: aws_sdk.Credentials,
+  bucketName: string,
+  artifactName: string
+) {
+  let params
+  params = {
+    apiVersion: 'latest',
+    region: 'us-east-1',
+    credentials
+  }
+  const s3Client = new aws_sdk.S3(params)
+  const codePipeline = new aws_sdk.CodePipeline(params)
+
+  params = {
+    Bucket: bucketName,
+    Prefix: artifactName,
+    MaxKeys: 1
+  }
   let objectID = s3Client.listObjectVersions(params).promise()
 
   params = {
     pipelineName: 'cicd-test',
     maxResults: 1
   }
-  let pipelineExecutions = codePipeline.listPipelineExecutions(params).promise();
-
-  const getStatus = async function () {
-    return await pipelineExecutions.then(data => data.pipelineExecutionSummaries?.at(0)?.status) || 'noresponse'
-  }
-
-  const workFlow = async function () {
-    let timeOut = 3
-    let ms = 5000
-    do {
-        if (await excutionRevisionCheck(credentials, bucketName, artifactName)){
-            console.log('Pipeline execution found!')
-            do {   
-                //console.log(`Pipeline current status: ${await getStatus()}`)
-                await wait(2000)
-            } while (await isRunning(credentials));
-            console.log('Pipeline execution status check done')
-            break
-        }
-        await wait(ms)
-        timeOut -= 1
-    }while(timeOut > 0)
-  }
-
-  workFlow()
-
+  let pipelineExecutions = codePipeline.listPipelineExecutions(params).promise()
+  let versionId = await objectID.then(data => data.Versions?.at(0)?.VersionId)
+  let revisionId = await pipelineExecutions.then(
+    data =>
+      data.pipelineExecutionSummaries?.at(0)?.sourceRevisions?.at(0)?.revisionId
+  )
+  if (versionId == revisionId) {
+    console.log(versionId, revisionId)
+    return true
+  } 
+  else return false
 }
 
-async function excutionRevisionCheck (credentials: aws_sdk.Credentials, bucketName: string, artifactName: string) {
-    let params
-    params = {
-      apiVersion: 'latest',
-      region: 'us-east-1',
-      credentials
-    }
-    const s3Client = new aws_sdk.S3(params)
-    const codePipeline = new aws_sdk.CodePipeline(params)
-
-    params = {
-        Bucket: bucketName,
-        Prefix: artifactName,
-        MaxKeys: 1
-    }
-    let objectID = s3Client.listObjectVersions(params).promise()
-
-    params = {
-        pipelineName: 'cicd-test',
-        maxResults: 1
-    }
-    let pipelineExecutions = codePipeline.listPipelineExecutions(params).promise();
-    let versionId = await objectID.then(data => data.Versions?.at(0)?.VersionId)
-    let revisionId = await pipelineExecutions.then(data => data.pipelineExecutionSummaries?.at(0)?.sourceRevisions?.at(0)?.revisionId)
-    if (versionId == revisionId)
-        return true 
-    else
-        return false
-}
-
-async function isRunning (credentials: aws_sdk.Credentials) {
-    let params
-    params = {
-      apiVersion: 'latest',
-      region: 'us-east-1',
-      credentials
-    }
-
-    const codePipeline = new aws_sdk.CodePipeline(params)
-    params = {
-        pipelineName: 'cicd-test',
-        maxResults: 1
-    }
-    let pipelineExecutions = codePipeline.listPipelineExecutions(params).promise();
-    let executionStatus = await pipelineExecutions.then(data => data.pipelineExecutionSummaries?.at(0)?.status) || 'noresponse'
-    let executionDetails = await pipelineExecutions.then(data => data)
-    let stopState = ['Succeeded', 'Stopped', 'Cancelled', 'Superseded', 'Failed', 'noresponse']
-    let isRunning = true
-    console.log(executionStatus, executionDetails.pipelineExecutionSummaries?.at(0)?.lastUpdateTime)
-    if (stopState.includes(executionStatus))
-        isRunning = false
-    return isRunning
+async function isRunning(credentials: aws_sdk.Credentials) {
+  let params
+  params = {
+    apiVersion: 'latest',
+    region: 'us-east-1',
+    credentials
   }
 
-  
-  
+  const codePipeline = new aws_sdk.CodePipeline(params)
+  params = {
+    pipelineName: 'cicd-test',
+    maxResults: 1
+  }
+  let pipelineExecutions = codePipeline.listPipelineExecutions(params).promise()
+  let executionStatus =
+    (await pipelineExecutions.then(
+      data => data.pipelineExecutionSummaries?.at(0)?.status
+    )) || 'noresponse'
+  let executionDetails = await pipelineExecutions.then(data => data)
+  let stopState = [
+    'Succeeded',
+    'Stopped',
+    'Cancelled',
+    'Superseded',
+    'Failed',
+    'noresponse'
+  ]
+  let isRunning = true
+  console.log(
+    executionStatus,
+    executionDetails.pipelineExecutionSummaries?.at(0)?.lastUpdateTime
+  )
+  if (stopState.includes(executionStatus)) isRunning = false
+  return isRunning
+}
